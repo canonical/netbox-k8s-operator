3303de6f103aa9e18275c09fb2935690b1b3c771
Ali Ugur <ali.ugur@canonical.com>
2025-09-02 12:21:06 +0300

    Chore: update to netbox-k8s

--- download_netbox.sh
+++ download_netbox.sh
@@ -5,22 +5,22 @@
 # This script downloads and extracts the NetBox source code.
 NETBOX_VERSION="4.3.6"
 wget https://github.com/netbox-community/netbox/archive/refs/tags/v${NETBOX_VERSION}.tar.gz
-if [ -d "netbox" ]; then
-    rm -rf netbox
+if [ -d "netbox-k8s" ]; then
+    rm -rf netbox-k8s
 fi
-mkdir netbox
-tar -xzvf v${NETBOX_VERSION}.tar.gz --strip-components=1 -C netbox
+mkdir netbox-k8s
+tar -xzvf v${NETBOX_VERSION}.tar.gz --strip-components=1 -C netbox-k8s
 patch -p1 <patches/settings.patch
 patch -p1 <patches/requirements.patch
 
 # Initiate Rockcraft 
-cp netbox_rockcraft.yaml netbox/rockcraft.yaml
+cp netbox_rockcraft.yaml netbox-k8s/rockcraft.yaml
 # Update the rockcraft.yaml with the correct version
-sed -i "s/^version: \".*\"/version: \"${NETBOX_VERSION}\"/" netbox/rockcraft.yaml
+sed -i "s/^version: \".*\"/version: \"${NETBOX_VERSION}\"/" netbox-k8s/rockcraft.yaml
 
 # Set up the cron job
-cp -r cron.d netbox/
+cp -r cron.d netbox-k8s/
 
-cp configuration.py netbox/netbox/netbox/configuration.py
+cp configuration.py netbox-k8s/netbox/netbox/configuration.py
 
-mv netbox/upgrade.sh netbox/migrate.sh
+mv netbox-k8s/upgrade.sh netbox-k8s/migrate.sh
