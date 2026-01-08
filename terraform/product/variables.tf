954facab5511fe10d458d12bec7a5082394d8725
Ali Ugur <ali.ugur@canonical.com>
2026-01-08 10:48:21 +0300

    Fix: model uuid thhing

--- terraform/product/variables.tf
+++ terraform/product/variables.tf
@@ -1,6 +1,11 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+variable "model" {
+  description = "Reference to the k8s Juju model to deploy application to."
+  type        = string
+}
+
 variable "model_uuid" {
   description = "Reference to the k8s Juju model's uuid to deploy application to."
   type        = string
